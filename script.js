1e2a9b72f701e8c3cf0c1966772fb12219804e10
Victor de Sousa Lemos <victorsousalemos@hotmail.com>
2021-06-30 16:35:55 -0300

    Aula-5 - Estrutura de Repetição - JS
    Aprendemos sobre For, While e Do While

---
+++ script.js
@@ -0,0 +1,27 @@
+var colors = ['black', 'white', 'yellow', 'green', 'blue']
+
+for (var i = 0; i < 5; i++) {
+  console.log(colors[i])
+}
+
+for (var i = 1; i <= 10; i++) {
+  console.log(i)
+}
+
+var i = 1
+
+while (i <= 10) {
+  console.log(i)
+  i++
+}
+
+do {
+  console.log(i)
+  i++
+} while (i < 10)
+
+// for = usa quando for array, matrizes e conjuntos
+
+// while = ele faz a verificação primeiro pra depois executar
+
+// do while = primeiro faz e depois de ter a rodada do bloco de codigo, ele vai verificar se a condição continua verdadeira pra continuar executando o loop do while.
