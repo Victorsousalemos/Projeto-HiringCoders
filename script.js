39fa090fb8d812a32e52d3dccc4373339884e52d
Victor de Sousa Lemos <victorsousalemos@hotmail.com>
2021-06-30 15:27:55 -0300

    Aula-3 - Operadores - JS
    Aprendemos sobre operadores aritméticos, operadores de atribuição. de comparação e lógicos.

---
+++ script.js
@@ -0,0 +1,53 @@
+//operadores aritméticos
+
+// adição +
+var sumTwoNumbers = 1 + 3
+console.log(sumTwoNumbers)
+// subtração -
+var subTwoNumbers = 4 - 3
+console.log(subTwoNumbers)
+// multiplicação *
+var multTwoNumbers = 4 * 3
+console.log(multTwoNumbers)
+// divisão /
+var divTwoNumbers = 4 / 2
+console.log(divTwoNumbers)
+// módulo ou resto de uma divisão %
+console.log(8 % 3)
+// incremento ++
+var num = 1
+console.log(++num)
+num = num + 1
+// decremento --
+var num = 2
+console.log(--num)
+num = num - 1
+
+//operadores de atribuição
+// = atribuição
+
+// atribuição de soma
+
+var x = 1
+var y = 3
+console.log((x += y))
+console.log((x -= y))
+console.log((x *= y))
+console.log((x /= y))
+console.log((x %= y))
+
+// é como se fosse x = x + y
+
+//operadores de comparação
+
+// ==
+// ===  comparar valores e tipos
+// !=  diferente de
+// < ou <=
+// > ou >=
+
+//operadores lógicos
+
+// e &&
+// ou ||
+// ! negação
