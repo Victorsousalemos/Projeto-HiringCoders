13cb8e2d607414ca18da68979c1ee1515bf48a8e
Victor de Sousa Lemos <victorsousalemos@hotmail.com>
2021-06-29 22:10:15 -0300

    Aula-2 - Tipo de Dados - JS
    Aprendemos na aula-2 os tipos de dados existentes no JavaScript, como strings, number, null, undefined, boolean, objetos, array, function e typeof

---
+++ script.js
@@ -0,0 +1,38 @@
+var myName = "Victor" //string
+
+var age = 25 //number
+
+var height = 1.83 //number
+
+null // objeto vazio, não possui dados
+undefined //dado ou valor que não foi definido
+
+var isStudent = false //boolean
+
+//Objeto, array e function
+
+//Objeto - mapeamento entre chaves (strings) e valores (qualquer tipo)
+
+var person ={
+  "name": "Victor", //com : e não ;
+  "age": 25
+}
+
+// arrays - conjunto de informações que representas listas ou conjuntos
+
+var students = [ "Victor", "Maria", "João"]
+
+// function - objeto que tem uma capacidade adicional, pode ser chamado ou executado
+
+function sum() {
+function sum(a,b) 
+  return a + b
+}
+
+//operador typeof - ajuda a identificar um tipo de dado dentro do js
+
+function sum(){
+  return 5 + 2
+}
+
+console.log(typeof sum)
