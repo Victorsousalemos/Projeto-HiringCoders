607d2cc90dcf4df709e408d2b4569a7f8d983d7d
Victor de Sousa Lemos <victorsousalemos@hotmail.com>
2021-06-30 17:21:37 -0300

    Aula-6 - Funções - JS
    Aprendemos sobre funções usando nome e soma.

---
+++ script.js
@@ -0,0 +1,11 @@
+function sayHello(name, lastName) {
+  console.log('Hello' + name + ' ' + lastName)
+}
+
+sayHello('Victor', 'Lemos')
+
+function sum(a, b) {
+  return a + b
+}
+
+console.log(sum(40, 60))
